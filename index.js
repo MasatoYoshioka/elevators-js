e3bf2550194b0262deb08fe38511ede252efcd67
masato yoshioka <yoshioka0627@gmail.com>
2016-10-25 00:37:16 +0900

    add

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 {
     init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
-        var elevator1 = elevators[1]
         // Whenever the elevator is idle (has no more queued destinations) ...
         for(elevator in elevators){
             this.handler(elevators[elevator]);
@@ -13,15 +11,16 @@
 
     update: function(dt, elevators, floors) {
         // We normally don't need to do anything here
+
     },
 
     handler: function(elevator){
         console.log(elevator)
         elevator.on("idle", function() {
-            elevator.goToFloor(0);
+            this.goToFloor(0);
         });
         elevator.on("floor_button_pressed", function(floorNum) {
-            elevator.goToFloor(floorNum)
+            this.goToFloor(floorNum)
         });
         elevator.on("passing_floor", function(floorNum, direction){
             console.log(direction)
